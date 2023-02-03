3b49c7630c6cf3bea379ecc4d831c9ab12bab6f9
Ed <gusdanieed@student.gu.se>
2023-02-03 21:50:50 +0100

    Planet export values
    Normalized too

--- scenes/Planet.gd
+++ scenes/Planet.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
-export(int, 1, 10) var rotation_speed = 1
+export(float, 0.00, 1.00, 0.01) var rotation_speed = 1
 enum Direction {CLOCKWISE = 1, COUNTERCLOCKWISE = -1}
 export(Direction) var direction
-# export(int, "Clockwise", "Counter-Clockwise") var direction = 1
+
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print(direction)
-	$RotationPlayer.playback_speed = rotation_speed * direction
+	$RotationPlayer.playback_speed = 0.01 + $RotationPlayer.playback_speed * rotation_speed * direction
 	
 	pass # Replace with function body.
 

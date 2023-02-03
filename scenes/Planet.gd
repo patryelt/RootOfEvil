af8cc168bfe39587b20e6a813dea5b9908156589
Ed <gusdanieed@student.gu.se>
2023-02-04 00:06:10 +0100

    Player collides and position at new planet

--- scenes/Planet.gd
+++ scenes/Planet.gd
@@ -4,17 +4,34 @@ export(float, 0.00, 1.00, 0.01) var rotation_speed = 1
 enum Direction {CLOCKWISE = 1, COUNTERCLOCKWISE = -1}
 export(Direction) var direction
 
+#When corruption is 0, the planet is not being currently attached to a player.
+export var corruption = 0
+
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(direction)
 	$RotationPlayer.playback_speed = 0.01 + $RotationPlayer.playback_speed * rotation_speed * direction
-	
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Area2D_area_entered(area):
+	if area.get_parent().name == "Player":
+		var player = area.get_parent()
+		if corruption <= 0:
+			player.planet = self
+			var newpos = player.global_position
+			get_parent().remove_child(player)
+			add_child(player)
+			
+			# new_parent.add_child(self)
+			corruption = 1
+			#set speed to 0?
+			player._jump_speed = 0
+			player.global_position = newpos
+		
+		
+		
+		
+		
+		print("Collided with '", self.name, "'!")
+		pass

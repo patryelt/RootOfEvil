0d051b1139754e49e7f236d0d67596654a63b828
Patrik Hjelt <patryelt@protonmail.com>
2023-02-04 15:26:02 +0100

    Use eye projectile

--- scenes/Player/PlayerSprite.gd
+++ scenes/Player/PlayerSprite.gd
@@ -2,11 +2,17 @@ extends Node2D
 
 func leave():
 	$eye_anim.hide()
+	$socket_anim.show()
 
 func jump():
 	$stem.hide()
 	$flower_anim.hide()
+	$eye_anim.hide()
+	$projectile.show()
 
 func grow():
 	$stem.show()
 	$flower_anim.show()
+	$projectile.hide()
+	$eye_anim.show()
+

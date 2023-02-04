ecbdb892fc353f9fd357a1ab69710beb6e88bdbc
Patrik Hjelt <patryelt@protonmail.com>
2023-02-04 13:42:05 +0100

    Set planet sprite

--- scenes/Planet.gd
+++ scenes/Planet.gd
@@ -3,6 +3,7 @@ extends Node2D
 export(float, 0.00, 1.00, 0.01) var rotation_speed = 1
 enum Direction {CLOCKWISE = 1, COUNTERCLOCKWISE = -1}
 export(Direction) var direction
+export(int, 7) var planet_sprite
 
 #When corruption is 0, the planet is not being currently attached to a player.
 export var corruption = 0
@@ -13,6 +14,8 @@ var _camera
 func _ready():
 	rotationPlayer.playback_speed = 0.01 + rotationPlayer.playback_speed * rotation_speed * direction
 	_camera = find_node("Camera2D")
+	$Area2D/PlanetSprite/planet_anim.frame = planet_sprite
+	
 
 func leave_planet():
 	rotationPlayer.playback_active = false
